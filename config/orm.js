a861898d337b36ddd8e9896e016a6abb1eaf34fe
Arod1207 <arod1207@me.com>
2020-07-13 23:39:25 -0500

    updated index.handlebars

--- config/orm.js
+++ config/orm.js
@@ -16,27 +16,27 @@ var orm = {
       cb(result);
     });
   },
-  // insertOne: function(whatToSelect, table, orderCol) {
-  //   var queryString = "INSERT INTO ?? FROM ?? ORDER BY ?? DESC";
-  //   console.log(queryString);
-  //   connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-  //     if (err) throw err;
-  //     console.log(result);
-  //   });
-  // },
-  // updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-  //   var queryString =
-  //     "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+  insertOne: function(whatToSelect, table, orderCol) {
+    var queryString = "INSERT INTO ?? FROM ?? ORDER BY ?? DESC";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    var queryString =
+      "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
 
-  //   connection.query(
-  //     queryString,
-  //     [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-  //     function(err, result) {
-  //       if (err) throw err;
-  //       console.log(result);
-  //     }
-  //   );
-  // }
+    connection.query(
+      queryString,
+      [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  }
 };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -9,17 +9,17 @@ var burger = {
       cb(res);
     });
   },
-  // // The variables cols and vals are arrays.
-  // insertOne: function(cols, vals, cb) {
-  //   orm.create("burgers", cols, vals, function(res) {
-  //     cb(res);
-  //   });
-  // },
-  // updateOne: function(objColVals, condition, cb) {
-  //   orm.update("burgers", objColVals, condition, function(res) {
-  //     cb(res);
-  //   });
-  // },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
 };
 
 module.exports = burger;
