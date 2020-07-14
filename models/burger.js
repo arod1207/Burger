84534f3f4a7442904c1f440208cff61e5d0c1052
Arod1207 <arod1207@me.com>
2020-07-13 21:28:14 -0500

    setup main.handlebars

--- models/burger.js
+++ models/burger.js
@@ -3,4 +3,23 @@ const orm = require('../config/orm');
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 
+var burger = {
+  all: function(cb) {
+    orm.all("cats", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insert: function(cols, vals, cb) {
+    orm.create("cats", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+};
+
 module.exports = burger;
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
-
+const exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+// Used to setup public page and use the css stylings
+app.use(express.static("public"));
+
+// Parse application body as JSON since we will be doing a post
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Setting up handlbars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//  Give access to routes 
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
