d42b20cd031ab1de9cb9a87c6e729edee9787b39
Arod1207 <arod1207@me.com>
2020-07-13 20:37:08 -0500

    created schema and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", true)
