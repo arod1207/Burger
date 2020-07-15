1eb37f29f18fa695dc38e121567ceabf536a866b
Arod1207 <arod1207@me.com>
2020-07-15 10:16:01 -0500

    deleted partial folder and file

--- models/burger.js
+++ models/burger.js
@@ -1,31 +1,22 @@
-// Import the ORM to create functions that will interact with the database.
+//importing the ORM to create the functions that will interact with the database
 var orm = require("../config/orm.js");
 
-
-// the name in "burgers" is the database name! //
-var cat = {
-  all: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll(function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (burger, cb) {
+        orm.insertOne(burger, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (id, cb) {
+        orm.updateOne([id], function (res) {
+            cb(res);
+        });
+    }
 };
 
-// Export the database functions for the controller (catsController.js).
-module.exports = cat;
+module.exports = burger;
