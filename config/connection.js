38cc083d348e312d0f13c65d25762937b137dcd5
Arod1207 <arod1207@me.com>
2020-07-16 00:21:26 -0500

    removed jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
 var mysql      = require('mysql');
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'Poorboi22',
   database : 'burgers_db'
 });
-} 
+
 
 connection.connect();
  
