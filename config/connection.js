0ec11ad01c5e4bd195d7581a89a5252604afc476
Arod1207 <arod1207@me.com>
2020-07-15 23:55:19 -0500

    add process.env.JAWSDB to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql      = require('mysql');
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'Poorboi22',
   database : 'burgers_db'
 });
- 
+} 
+
 connection.connect();
  
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
