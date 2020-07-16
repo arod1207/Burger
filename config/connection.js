a614158a23b7e69a93d553a5700962bd950e4bbd
Arod1207 <arod1207@me.com>
2020-07-16 10:19:19 -0500

    modified connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Poorboi22',
+  password : '*****',
   database : 'burgers_db'
 });
 
