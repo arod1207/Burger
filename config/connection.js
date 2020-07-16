3bfe5e1b38e252c3954804350ca934d1c0f1f2f7
Arod1207 <arod1207@me.com>
2020-07-16 12:56:07 -0500

    added JAWSDBURL

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 var mysql      = require('mysql');
 
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Poorboi22',
+  password : 'Devtest123',
   database : 'burgers_db'
 });
+}
 
 
 connection.connect();
